f65a9cd2617581517112dfc6bdea2abd71c647c4
Caio <caiiochaves82@gmail.com>
2024-08-22 13:20:45 -0300

    :recycle: refactor: css display flex corrections

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -4,8 +4,10 @@ import styles from './Welcome.module.css'
 export function Welcome() {
     return (
         <div className={styles.mainDiv}>
-            <h1>Want to make your commerce faster?</h1>
+            <h1>Do you want to make your business more efficient?</h1>
+            <p>Try our solutions!</p>
             <img src={myImage} alt="Description of the SVG"/>
+
         </div>
     )
 }
--- src/style/HomeStyle.js
+++ src/style/HomeStyle.js
@@ -11,8 +11,9 @@ body {
     display: flex;
     margin-top: 17vh;
     align-items: center;
-    justify-content: center;
+    justify-content: start;
     flex-direction: column;
-    background-color: #5990e3;
+    background: rgb(42,68,103);
+    background: radial-gradient(circle, rgba(42,68,103,1) 0%, rgba(39,56,82,1) 100%);
 }
 `;
